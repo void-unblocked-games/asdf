77d8d202d99cfe0c3d7cf5020522bd08392bbf11
System Administrator <root@Kims-MacBook-Air.local>
2025-07-10 13:51:18 -0400

    feat: Populate new display name input with current vanity

--- client.js
+++ client.js
@@ -379,8 +379,8 @@ settingsButton.addEventListener('click', () => {
         settingsButton.classList.remove('jiggle');
     }, 300);
 
-    // Set the current display name in the dropdown
-    document.getElementById('current-display-name').textContent = myUserVanity;
+    // Set the current display name in the input field
+    newDisplayNameInput.value = myUserVanity;
 
     // Toggle the settings dropdown
     settingsDropdown.classList.toggle('show');
